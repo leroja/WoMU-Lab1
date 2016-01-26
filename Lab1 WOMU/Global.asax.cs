fee644073d84e0deb0d77f4cc2905ee175647e38
Lennart Jakobsson <len.nart@hotmail.com>
2016-01-26 14:28:53 +0100

    all code from tutorial with minor changes

--- Lab1 WOMU/Global.asax.cs
+++ Lab1 WOMU/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Lab1_WOMU
@@ -13,6 +14,7 @@ namespace Lab1_WOMU
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
