53ea04f8f0a4f5ed499eba86c907faaf4abfd936
Lennart Jakobsson <len.nart@hotmail.com>
2016-01-28 14:05:31 +0100

    Changes to Produkts

--- Lab1 WOMU/Controllers/ProduktController.cs
+++ Lab1 WOMU/Controllers/ProduktController.cs
@@ -35,85 +35,13 @@ namespace Lab1_WOMU.Controllers
             return View(produkt);
         }
 
-        // GET: Produkt/Create
-        public ActionResult Create()
+
+        // GET: Produkt
+        public ActionResult BuyPage()
         {
             return View();
         }
 
-        // POST: Produkt/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProduktID,ProduktNamn,ProduktBeskrivning,Pris,AntalILager")] Produkt produkt)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Produkter.Add(produkt);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(produkt);
-        }
-
-        // GET: Produkt/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Produkt produkt = db.Produkter.Find(id);
-            if (produkt == null)
-            {
-                return HttpNotFound();
-            }
-            return View(produkt);
-        }
-
-        // POST: Produkt/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProduktID,ProduktNamn,ProduktBeskrivning,Pris,AntalILager")] Produkt produkt)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(produkt).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(produkt);
-        }
-
-        // GET: Produkt/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Produkt produkt = db.Produkter.Find(id);
-            if (produkt == null)
-            {
-                return HttpNotFound();
-            }
-            return View(produkt);
-        }
-
-        // POST: Produkt/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Produkt produkt = db.Produkter.Find(id);
-            db.Produkter.Remove(produkt);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
 
         protected override void Dispose(bool disposing)
         {
